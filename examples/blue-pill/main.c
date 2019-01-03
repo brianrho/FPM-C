b59ac08154f542885721393becb4aeb71b462020
brianrho <brianrho94@gmail.com>
2019-01-03 08:12:05 +0000

    Add read_template and match_prints examples

--- examples/blue-pill/main.c
+++ examples/blue-pill/main.c
@@ -6,6 +6,7 @@
 #include "fpm.h"
 
 #include <stdio.h>
+#include <ctype.h>
 
 /* handles for UART comms */
 uart_t uart1_dev = {USART1};
@@ -21,6 +22,12 @@ FPM_System_Params params;
 
 void enroll_finger(int16_t fid);
 uint8_t get_free_id(int16_t * fid);
+uint16_t read_template(uint16_t fid, uint8_t * buffer, uint16_t buff_sz);
+void match_prints(int16_t fid, int16_t otherfid);
+
+void enroll_mainloop(void);
+void templates_mainloop(void);
+void matchprints_mainloop(void);
 
 void gpio_clocks_enable(void) {
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA
@@ -61,6 +68,14 @@ int main(void)
 		while (1);
 	}
 
+    /* uncomment only one to test */
+    enroll_mainloop();
+    //templates_mainloop();
+	//matchprints_mainloop();
+}
+
+/* main loop for enroll example */
+void enroll_mainloop(void) {
 	while (1) {
 		printf("Send any character to enroll a finger...\r\n");
 		while (uart_avail(&uart1_dev) == 0);
@@ -72,7 +87,36 @@ int main(void)
 			printf("No free slot in flash library!\r\n");
 		while (uart_read_byte(&uart1_dev) != -1);  // clear buffer
 	}
+}
 
+/* this is equal to the template size for the fingerprint module,
+ * some modules have 512-byte templates while others have
+ * 768-byte templates. Check the printed result of read_template()
+ * to determine the case for your module and adjust the needed buffer
+ * size below accordingly */
+#define BUFF_SZ     512
+
+uint8_t template_buffer[BUFF_SZ];
+
+/* main loop for templates example */
+void templates_mainloop(void) {
+	while (1) {
+		printf("Enter the template ID # you want to read...");
+		int16_t fid = 0;
+		while (uart_read_byte(&uart1_dev) != -1);
+		while (1) {
+			while (uart_avail(&uart1_dev) == 0);
+			char c = uart_read_byte(&uart1_dev);
+			if (! isdigit(c)) break;
+			fid *= 10;
+			fid += c - '0';
+		}
+
+		/* read the template from its location into the buffer */
+		uint16_t total_read = read_template(fid, template_buffer, BUFF_SZ);
+		if (!total_read)
+			return;
+	}
 }
 
 /* get free ID in sensor database */
@@ -94,6 +138,18 @@ uint8_t get_free_id(int16_t * fid) {
     }
 
     return 0;
+}
+
+void matchprints_mainloop(void) {
+	int16_t fid = 18;
+	int16_t otherfid = 16;
+
+	while (1) {
+		printf("Send any character to match the 2 predefined IDs...\r\n");
+		while (uart_avail(&uart1_dev) == 0);
+		match_prints(fid, otherfid);
+		while (uart_read_byte(&uart1_dev) != -1);  // clear buffer
+	}
 }
 
 void enroll_finger(int16_t fid) {
@@ -184,6 +240,105 @@ void enroll_finger(int16_t fid) {
 			printf("Error code: 0x%X!\r\n", p);
 			return;
 	}
+}
+
+uint16_t read_template(uint16_t fid, uint8_t * buffer, uint16_t buff_sz) {
+    int16_t p = fpm_load_model(&finger, fid, 1);
+    switch (p) {
+        case FPM_OK:
+            printf("Template %d loaded.\r\n", fid);
+            break;
+        default:
+			printf("Error code: 0x%X!\r\n", p);
+			return 0;
+    }
+
+    // OK success!
+
+    p = fpm_get_model(&finger, 1);
+    switch (p) {
+        case FPM_OK:
+            break;
+        default:
+			printf("Error code: 0x%X!\r\n", p);
+			return 0;
+    }
+
+    uint8_t read_finished;
+    int16_t count = 0;
+    uint16_t readlen = buff_sz;
+    uint16_t pos = 0;
+
+    while (1) {
+        uint8_t ret = fpm_read_raw(&finger, FPM_OUTPUT_TO_BUFFER, buffer + pos, &read_finished, &readlen);
+        if (ret) {
+            count++;
+            pos += readlen;
+            readlen = buff_sz - pos;
+            if (read_finished)
+                break;
+        }
+        else {
+            printf("Error receiving packet %d\r\n", count);
+            return 0;
+        }
+    }
+
+    uint16_t total_bytes = count * fpm_packet_lengths[params.packet_len];
+
+    /* just for pretty-printing */
+    uint16_t num_rows = total_bytes / 16;
+
+    printf("Printing template:\r\n");
+    printf("---------------------------------------------\r\n");
+    for (int row = 0; row < num_rows; row++) {
+        for (int col = 0; col < 16; col++) {
+            printf("%d, ", buffer[row * 16 + col]);
+        }
+        printf("\r\n");
+    }
+    printf("--------------------------------------------\r\n");
+
+    printf("%d bytes read.\r\n", total_bytes);
+    return total_bytes;
+}
+
+void match_prints(int16_t fid, int16_t otherfid) {
+    /* read template into slot 2 */
+	int16_t p = fpm_load_model(&finger, fid, 1);
+	switch (p) {
+		case FPM_OK:
+			printf("Template %d loaded.\r\n", fid);
+			break;
+		default:
+			printf("Error code: 0x%X!\r\n", p);
+			return;
+	}
+
+    /* read template into slot 2 */
+    p = fpm_load_model(&finger, otherfid, 2);
+    switch (p) {
+        case FPM_OK:
+            printf("Template %d loaded.\r\n", otherfid);
+            break;
+        default:
+			printf("Error code: 0x%X!\r\n", p);
+			return;
+    }
+
+    uint16_t match_score = 0;
+    p = fpm_match_template_pair(&finger, &match_score);
+    switch (p) {
+        case FPM_OK:
+            printf("Match score: %d\r\n", match_score);
+            break;
+        case FPM_NOMATCH:
+            printf("Prints did not match.\r\n");
+            break;
+        default:
+			printf("Error code: 0x%X!\r\n", p);
+			return;
+    }
 }
 
 /* definitions for uart3 control */
